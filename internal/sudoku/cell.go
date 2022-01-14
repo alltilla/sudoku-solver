b94a4d4d47a0972964cf2da0ca7357fb369f4c22
Attila Szakacs <szakacs.attila96@gmail.com>
2022-01-14 12:08:29 +0100

    sudoku/cell: add class, ctor and GetValue()
    Signed-off-by: Attila Szakacs <szakacs.attila96@gmail.com>

--- internal/sudoku/cell.go
+++ internal/sudoku/cell.go
@@ -1,1 +1,21 @@
 package sudoku
+
+const Empty int = -127
+
+type Cell struct {
+	value int
+}
+
+func NewCell() *Cell {
+	value := Empty
+
+	c := Cell{
+		value,
+	}
+
+	return &c
+}
+
+func (c *Cell) GetValue() int {
+	return c.value
+}
--- internal/sudoku/cell_test.go
+++ internal/sudoku/cell_test.go
@@ -1,1 +1,17 @@
 package sudoku
+
+import (
+	"testing"
+)
+
+func assertValue(t *testing.T, actual int, expected int) {
+	if expected != actual {
+		t.Errorf("unexpected value. expected: %d, actual: %d", expected, actual)
+	}
+}
+
+func TestGetValueDefault(t *testing.T) {
+	cell := NewCell()
+
+	assertValue(t, cell.GetValue(), Empty)
+}
