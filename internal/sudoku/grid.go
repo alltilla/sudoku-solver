befafa6327f9dc1c97e5b673737a0d3811322334
Attila Szakacs <szakacs.attila96@gmail.com>
2022-01-14 12:34:46 +0100

    sudoku/grid: add ctor
    Signed-off-by: Attila Szakacs <szakacs.attila96@gmail.com>

--- internal/sudoku/grid.go
+++ internal/sudoku/grid.go
@@ -1,1 +1,17 @@
 package sudoku
+
+type Grid struct {
+	cells [9][9]*Cell
+}
+
+func NewGrid() *Grid {
+	g := Grid{}
+
+	for row := 0; row < 9; row++ {
+		for column := 0; column < 9; column++ {
+			g.cells[row][column] = NewCell()
+		}
+	}
+
+	return &g
+}
