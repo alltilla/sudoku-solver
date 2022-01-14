d9ce3f09d4348b5cf7a1345d92505064f8911b0d
Attila Szakacs <szakacs.attila96@gmail.com>
2022-01-14 12:37:02 +0100

    sudoku/grid: add GetCell()
    Signed-off-by: Attila Szakacs <szakacs.attila96@gmail.com>

--- internal/sudoku/grid.go
+++ internal/sudoku/grid.go
@@ -1,5 +1,9 @@
 package sudoku
 
+import (
+	"fmt"
+)
+
 type Grid struct {
 	cells [9][9]*Cell
 }
@@ -14,4 +18,29 @@ func NewGrid() *Grid {
 	}
 
 	return &g
+}
+
+func checkRowAndColumnValidity(row int, column int) error {
+	if row > 9 {
+		return fmt.Errorf("row is larger than 9: %d", row)
+	}
+	if row < 1 {
+		return fmt.Errorf("row is smaller than 1: %d", row)
+	}
+	if column > 9 {
+		return fmt.Errorf("column is larger than 9: %d", column)
+	}
+	if column < 1 {
+		return fmt.Errorf("column is smaller than 1: %d", column)
+	}
+
+	return nil
+}
+
+func (g *Grid) GetCell(row int, column int) (*Cell, error) {
+	err := checkRowAndColumnValidity(row, column)
+	if err != nil {
+		return nil, err
+	}
+	return g.cells[row-1][column-1], nil
 }
--- internal/sudoku/grid_test.go
+++ internal/sudoku/grid_test.go
@@ -1,1 +1,49 @@
 package sudoku
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/alltilla/sudoku-solver/internal/test_utils"
+)
+
+func TestGetCell(t *testing.T) {
+	for row := 1; row <= 9; row++ {
+		for column := 1; column <= 9; column++ {
+			t.Run(fmt.Sprintf("%d,%d", row, column), func(t *testing.T) {
+				grid := NewGrid()
+
+				cell, err := grid.GetCell(row, column)
+				AssertNoError(t, err)
+
+				if cell == nil {
+					t.Errorf("missing cell")
+				}
+			})
+		}
+	}
+}
+
+func TestGetCellInvalid(t *testing.T) {
+	coordinates_to_get := [][2]int{
+		{0, 1},
+		{1, 0},
+		{10, 1},
+		{1, 10},
+	}
+
+	for _, coordinate_to_get := range coordinates_to_get {
+		row := coordinate_to_get[0]
+		column := coordinate_to_get[1]
+
+		t.Run(fmt.Sprintf("%d,%d", row, column), func(t *testing.T) {
+			grid := NewGrid()
+
+			cell, err := grid.GetCell(row, column)
+			AssertError(t, err)
+			if cell != nil {
+				t.Errorf("unexpected cell")
+			}
+		})
+	}
+}
